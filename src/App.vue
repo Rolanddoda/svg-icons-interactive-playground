b865fc66f959aae5cf99f9072af55ad6fa2b690d
Roland Doda <rolanddoda2014@gmail.com>
2022-04-16 15:17:48 +0200

    CSS Variables section && responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { useGlobalStore } from '@/stores/global-store'
 // Components
+import ControlsSection from '@/components/ControlsSection.vue'
 import CodeSnippet from '@/components/CodeSnippet.vue'
 import ResultSection from '@/components/ResultSection.vue'
-import ControlsSection from '@/components/ControlsSection.vue'
+import VariablesSection from '@/components/VariablesSection.vue'
 
 const globalStore = useGlobalStore()
 </script>
@@ -13,10 +14,11 @@ const globalStore = useGlobalStore()
     <div class="gradient-font q-my-sm">Unified way of using SVG Icons</div>
   </header>
 
-  <main class="">
-    <ResultSection />
-    <CodeSnippet />
-    <ControlsSection />
+  <main class="main" :class="`screen-${$q.screen.name}`">
+    <ResultSection class="result-section" style="grid-area: result" />
+    <CodeSnippet class="code-snippet" style="grid-area: code" />
+    <ControlsSection class="controls-section" style="grid-area: controls" />
+    <VariablesSection class="variables-section" style="grid-area: variables" />
   </main>
 </template>
 
@@ -32,5 +34,36 @@ const globalStore = useGlobalStore()
   --q-warning: v-bind('globalStore.cssVarColors.warning');
 
   width: 100%;
+  display: grid;
+  grid-template-areas:
+    'code'
+    'result'
+    'controls'
+    'variables';
+  gap: 12px;
+
+  &.screen-xs,
+  &.screen-sm {
+    .code-snippet {
+      padding: 0 4px;
+    }
+  }
+
+  &.screen-md {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    grid-template-areas:
+      'result code'
+      'controls variables';
+  }
+
+  &.screen-lg,
+  &.screen-xl {
+    display: grid;
+    grid-template-columns: 1fr minmax(500px, 1fr) 1fr;
+    grid-template-areas:
+      'controls code variables'
+      'controls result variables';
+  }
 }
 </style>
---
+++ src/components/VariablesSection.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useGlobalStore } from '@/stores/global-store'
+
+const globalStore = useGlobalStore()
+</script>
+
+<template>
+  <div class="css-vars-area gradient-border relative-position q-pa-xl">
+    <h4 class="h4 text-indigo-3 text-center q-mb-md">CSS Variables</h4>
+
+    <q-input
+      v-for="(colorValue, colorKey) in globalStore.cssVarColors"
+      v-model="globalStore.cssVarColors[colorKey]"
+      :key="colorKey"
+      filled
+      dark
+      dense
+      class="my-input q-mb-sm"
+    >
+      <template #prepend>
+        <q-icon name="circle" :color="colorKey"></q-icon>
+        <small> {{ colorKey }}:</small>
+      </template>
+      <template #append>
+        <q-icon name="colorize" class="cursor-pointer">
+          <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+            <q-color dark v-model="globalStore.cssVarColors[colorKey]" />
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+    </q-input>
+  </div>
+</template>
